ae6e60fd40879b7ad7b1de3b91d952cea18ff8d8
DiogenSin <jakasdovydas@gmail.com>
2023-02-12 13:21:27 +0200

    Fixed registration, edit/delete tik logged in

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,19 +1,52 @@
 import { NavLink } from "react-router-dom"
+import UserContext from "../contexts/UserContexts"
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
 
+    const { setLoggedIn, loggedIn, loggedInUser } = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        setLoggedIn(false)
+        navigate('/')
+    }
+
+    const handleNewQuestion = () => {
+        console.log('Noris sukurti naują klausimą')
+    }
 
     return (
+        <>
+        {
+            loggedIn ?
 
-        <header>
-            <a href="/"><img src="https://www.freeiconspng.com/thumbs/logo-design/pink-blue-logo-design-template-png-6.png" alt="" /></a>
+                <header>
+                    <a href="/"><img src="https://www.freeiconspng.com/thumbs/logo-design/pink-blue-logo-design-template-png-6.png" alt="" /></a>
+                    <div id="userField">
+                        <i className="fa fa-plus-square" onClick={handleNewQuestion}></i>
+                        <div id="userInfo">
+                            <p id="userName">{loggedInUser.name}</p>
+                            <p id="logOut" onClick={handleLogout}>Logout</p>
+                        </div>
+                        <img src={loggedInUser.picture} alt="" />
+                    </div>
+                </header>
+            :
+                <header>
+                    <a href="/"><img src="https://www.freeiconspng.com/thumbs/logo-design/pink-blue-logo-design-template-png-6.png" alt="" /></a>
+                    <nav>
+                        <NavLink to='/login'>Login</NavLink>
+                        <button id='registrationBtn'><NavLink to='/registration'>Registration</NavLink></button>
+                    </nav>
+                </header>
+        }
 
-            <nav>
-                <NavLink to='/login'>Login</NavLink>
-                <button id='registrationBtn'><NavLink to='/registration'>Registration</NavLink></button>
-            </nav>
+        
+        </>
 
-        </header>
 
     )
 }
